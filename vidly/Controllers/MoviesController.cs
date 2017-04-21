14d4b02a7933d45a70d8cce7ac405004cbeda204
mc-carthy <mccarthy.mick0012@gmail.com>
2017-04-21 17:22:32 +0100

    Testing optional action parameters

--- vidly/Controllers/MoviesController.cs
+++ vidly/Controllers/MoviesController.cs
@@ -19,5 +19,25 @@ namespace vidly.Controllers
             //return new EmptyResult();
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
