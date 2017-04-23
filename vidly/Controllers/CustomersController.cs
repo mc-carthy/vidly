cdffd5b2c5a90661971ba27fcbb0c44815064e71
mc-carthy <mccarthy.mick0012@gmail.com>
2017-04-23 20:39:50 +0100

    Querying database for customers

--- vidly/Controllers/CustomersController.cs
+++ vidly/Controllers/CustomersController.cs
@@ -9,17 +9,28 @@ namespace vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -27,14 +38,6 @@ namespace vidly.Controllers
             }
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer> {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Smith" }
-            };
         }
     }
 }
