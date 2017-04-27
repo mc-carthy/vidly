78d3e3ae03f58ddfbe2811783c8a87472cb7d49b
mc-carthy <mccarthy.mick0012@gmail.com>
2017-04-27 22:08:21 +0100

    Create an edit form (which re-adds the customer to the database when saved...)

--- vidly/Controllers/CustomersController.cs
+++ vidly/Controllers/CustomersController.cs
@@ -26,12 +26,12 @@ namespace vidly.Controllers
         public ActionResult New ()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -60,6 +60,24 @@ namespace vidly.Controllers
             }
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
