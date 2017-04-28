2dd87437a0bb0b2f8610825175760d014f49b2ee
mc-carthy <mccarthy.mick0012@gmail.com>
2017-04-28 21:56:01 +0100

    Refactor magic numbers by defining membership type values in model

--- vidly/Models/Min18YearsIfAMember.cs
+++ vidly/Models/Min18YearsIfAMember.cs
@@ -12,7 +12,8 @@ namespace vidly.Models
         {
             var customer = (Customer)validationContext.ObjectInstance;
 
-            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            if (customer.MembershipTypeId == MembershipType.Unkown || 
+                customer.MembershipTypeId == MembershipType.PayAsYouGo)
             {
                 return ValidationResult.Success;
             }
