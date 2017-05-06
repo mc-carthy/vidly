a5758788937612b2d19e5d5ec7242a642c1f4795
mc-carthy <mccarthy.mick0012@gmail.com>
2017-05-06 17:13:31 +0100

    Slacken validation to ensure greater readability for non-critical elements

--- vidly/Controllers/Api/NewRentalsController.cs
+++ vidly/Controllers/Api/NewRentalsController.cs
@@ -22,26 +22,11 @@ namespace vidly.Controllers.Api
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
 
-            if (newRental.MovieIds.Count == 0)
-            {
-                return BadRequest("No movie Ids have been given");
-            }
-
-            var customer = _context.Customers.SingleOrDefault(
+            var customer = _context.Customers.Single(
                 c => c.Id == newRental.CustomerId);
-
-            if (customer == null)
-            {
-                return BadRequest("CustomerId is not valid.");
-            }
 
             var movies = _context.Movies.Where(
                 m => newRental.MovieIds.Contains(m.Id)).ToList();
-
-            if (movies.Count != newRental.MovieIds.Count)
-            {
-                return BadRequest("One or more MovieIds are invalid.");
-            }
 
             foreach(var movie in movies)
             {
